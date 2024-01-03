e82b9999bec19e41b169b13262161a9a812dd9fc
abhishekkashyap78 <abhishekkashyap8755679321@gmail.com>
2024-01-03 11:14:31 +0530

    design complete

--- busbookingapp/src/Main.java
+++ busbookingapp/src/Main.java
@@ -1,9 +1,23 @@
+import javax.swing.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// then press Enter. You can now see whitespace character
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        JFrame frame = new JFrame("busbookingapp");
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setSize(700,600);
+        frame.setVisible(true);
+
+        JLabel namelb = new JLabel("Name");
+        JLabel emaillb = new JLabel("Email Id");
+        JLabel mmobilelb = new JLabel("Moblie");
+        JLabel addresslb = new JLabel("Adress");
+        JLabel sourcelb = new JLabel("Source");
+        JLabel destinationlb = new JLabel("Distination");
+        JLabel paymentlb = new JLabel("Payment");
+        JLabel genderlb = new JLabel("gender");
 
     }
 }
